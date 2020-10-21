b3bc98d983951eb9f2c1f2ecab695901e76275d7
Tikka710 <norsupyssy1020@gmail.com>
2020-10-21 20:17:17 +0900

    updata other

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -8,7 +8,11 @@
   <article-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'>
   </article-tags-input>
 </div>
+
+
 <div class="form-group">
+
   <label></label>
+  <block-editor></block-editor>
 　　<textarea name="body" required class="form-control" rows="6" placeholder="説明を入力してください">{{ $article->body ?? old('body') }}</textarea>
 </div>
